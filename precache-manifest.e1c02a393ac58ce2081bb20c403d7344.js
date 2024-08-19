44a5be1fcf8a9806d89065b94f18d8a8eb7bb3b0
Max Fibra <contato@grupomaxltda.com.br>
2024-08-19 17:23:05 -0300

    Updates

---
+++ precache-manifest.e1c02a393ac58ce2081bb20c403d7344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7ec0d71ba7b896c9a96985b3bacdc7",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "ba35d5a87598334ab3c5",
+    "url": "/admin/static/css/2.2a42abf6.chunk.css"
+  },
+  {
+    "revision": "6b2a587a8e743d5a7e1b",
+    "url": "/admin/static/css/main.db4fc924.chunk.css"
+  },
+  {
+    "revision": "ba35d5a87598334ab3c5",
+    "url": "/admin/static/js/2.519db654.chunk.js"
+  },
+  {
+    "revision": "1a10a6962762401eac9d",
+    "url": "/admin/static/js/3.8c854cc2.chunk.js"
+  },
+  {
+    "revision": "6b2a587a8e743d5a7e1b",
+    "url": "/admin/static/js/main.a2043dcb.chunk.js"
+  },
+  {
+    "revision": "3a294d2f6ba6978a89ef",
+    "url": "/admin/static/js/runtime~main.f6d78e8d.js"
+  }
+]);
